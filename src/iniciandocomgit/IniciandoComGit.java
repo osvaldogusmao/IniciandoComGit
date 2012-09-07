73f982615879038cd2e82053ae7086f5ebf08830
Anderson Luis Ribeiro <anderson.luis.rib@gmail.com>
2012-09-06 21:34:47 -0300

    Alteracao qualquer...

--- src/iniciandocomgit/IniciandoComGit.java
+++ src/iniciandocomgit/IniciandoComGit.java
@@ -10,10 +10,12 @@ package iniciandocomgit;
  */
 public class IniciandoComGit {
 
-    /**
+    /**d 
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.println("Imprimindo...");
+        
     }
 }
